6ec46440f0531147725a04cd3e65c3bea898f700
Angeliss <59443350+lion-goose@users.noreply.github.com>
2021-01-29 11:05:51 +0800

    Update Youth_Read.js

--- Task/Youth_Read.js
+++ Task/Youth_Read.js
@@ -1,14 +1,11 @@
 /*
 更新时间: 2020-09-26 8:46
 Github Actions使用方法见[@lxk0301](https://raw.githubusercontent.com/lxk0301/scripts/master/githubAction.md) 使用方法大同小异
-
 请自行抓包，阅读文章和看视频，倒计时转一圈显示青豆到账即可，多看几篇文章和视频，获得更多包数据，抓包地址为"https://ios.baertt.com/v5/article/complete.json"，在Github Actions中的Secrets新建name为'YOUTH_READ'的一个值，拷贝抓包的请求体到下面Value的文本框中，添加的请求体越多，获得青豆次数越多，本脚本不包含任何推送通知
-
 多个请求体时用'&'号或者换行隔开" ‼️
-
 */
 
-let s = 30000 //等待延迟30s
+//let s = 30000 //等待延迟30s
 const $ = new Env("中青看点")
 //const notify = $.isNode() ? require('./sendNotify') : '';
 let ReadArr = [], YouthBody = "",readscore = 0;
@@ -40,7 +37,7 @@ let ReadArr = [], YouthBody = "",readscore = 0;
       $.index = i + 1;
       console.log(`-------------------------\n\n开始中青看点第${$.index}次阅读`)
     }
-  await AutoRead();
+      await AutoRead();
  }
    console.log(`-------------------------\n\n中青看点共完成${$.index}次阅读，共计获得${readscore}个青豆，阅读请求全部结束`)
 })()
@@ -50,7 +47,6 @@ let ReadArr = [], YouthBody = "",readscore = 0;
 
 function AutoRead() {
     return new Promise((resolve, reject) => {
-      setTimeout(()=> {
        let url = {
             url: `https://ios.baertt.com/v5/article/complete.json`,
             headers: {
@@ -58,12 +54,13 @@ function AutoRead() {
             },
             body: articlebody
         };
-        $.post(url, (error, response, data) => {
+        $.post(url, async(error, response, data) => {
            let readres = JSON.parse(data);
-             console.log(data)
+             //console.log(data)
            if (readres.error_code == '0' && typeof readres.items.read_score === 'number') {
-              console.log(`\n本次阅读获得${readres.items.read_score}个青豆，即将开始下次阅读\n`)
-              readscore += readres.items.read_score
+              console.log(`\n本次阅读获得${readres.items.read_score}个青豆，请等待30s后执行下一次阅读\n`);
+              readscore += readres.items.read_score;
+              await $.wait(30000);
             }
             else if (readres.error_code == '0' && typeof readres.items.score === 'number') {
               console.log(`\n本次阅读获得${readres.items.score}个青豆，即将开始下次阅读\n`)
@@ -77,7 +74,6 @@ function AutoRead() {
             }
           resolve()
         })
-      },s)
     })
 }
 
